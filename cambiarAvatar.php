f29df86dfbe44170ccdbe5ecb589f025554aca8f
EricM76 <menaericdaniel@gmail.com>
2019-10-24 21:11:04 -0300

    perfil completo

--- cambiarAvatar.php
+++ cambiarAvatar.php
@@ -1,24 +1,14 @@
 <?php
 include_once("autoload.php");
 if (isset($_GET["avatar"])) {
-  // $avatar=$_GET["avatar"];
-  // $id=$_GET["id"];
-  // $sql = "UPDATE usuarios SET avatar ='$avatar' WHERE id ='$id'";
-  // $query = $pdo->prepare($sql);
-  // $query->execute();
-  // header("location:registros.php");
   ArmarRegistro::cambiarAvatar($_GET["id"], $_GET["avatar"],$pdo);
   header("location:registros.php");
 }
-  // $id = $_POST["id"];
-  // $sql = "SELECT perfil FROM usuarios WHERE id LIKE $id";
-  // $query = $pdo->prepare($sql);
-  // $query->execute();
-  // $perfil = $query->fetch(PDO::FETCH_ASSOC);
-  // unlink("images/perfil/.".$perfil["perfil"]);
-  // exit;
+if ($_POST) {
   ArmarRegistro::eliminarImgPerfil($pdo,$_POST["id"]);
   ArmarRegistro::guardarPerfil($pdo,$_POST["id"],$_FILES);
   header("location:registros.php");
+}
+
 
 ?>
--- registros.php
+++ registros.php
@@ -10,77 +10,78 @@ include("autoload.php");
    </head>
    <?php include("header.php") ?>
    <body class="">
-     <div class="container-fluid mt-3">
-       <?php
-       $registros=BaseMySQL::verRegistros($pdo,'usuarios');
-       ?>
-       <table class="table table-
-       light small">
-        <thead>
-          <tr>
-            <th scope="col">id</th>
-            <th scope="col">nombre</th>
-            <th scope="col">apellido</th>
-            <th scope="col">email</th>
-            <th scope="col">pass</th>
-            <th scope="col">fecha</th>
-            <th scope="col">sexo</th>
-            <th scope="col">avatar</th>
-            <th scope="col">perfil</th>
-            <th scope="col">valoracion</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($registros as $registro): ?>
-          <tr>
-            <th scope="row"><a href="perfil.php?id=<?=$registro["id"]?>"><?=$registro["id"]?></a></th>
-            <td><?=$registro["nombre"]?></td>
-            <td><?=$registro["apellido"]?></td>
-            <td><?=$registro["email"]?></td>
-            <td><?=$registro["pass"]?></td>
-            <td><?=$registro["fecha"]?></td>
-            <td><?=$registro["sexo"]?></td>
-            <td>
-              <?php
-                if ($registro["avatar"]!=null) {
-                  ?>
-                  <a href="registros.php?id=<?=$registro["id"]?>"><?=$registro["avatar"]?></a>
-                  <?php
-                  // echo $registro["avatar"];
-                }else {?>
-                  <a href="registros.php?id=<?=$registro["id"]?>">agregar</a>
-
-              <?php } ?>
-            </td>
-
-            <td>
-              <?php
-                if ($registro["perfil"]!=null) {
-                  echo $registro["perfil"];
-                }else {?>
-                  <a href="#">agregar</a>
-              <?php } ?>
-            </td>
-
-            <td><?=$registro["val_user"]?></td>
-          </tr>
-        <?php endforeach; ?>
-        </tbody>
-       </table>
-     </div>
-
-
+   <div class="container-fluid mt-3">
      <?php
-     include_once("editarAvatar.php");
-     include_once("javascript.php");
+     $registros=BaseMySQL::verRegistros($pdo,'usuarios');
      ?>
-     <?php if (isset($_GET["id"])): ?>
-       <script type="text/javascript">
-        $(function(){
-         $("#avatar").modal();
-        });
-       </script>
-     <?php endif; ?>
+     <table class="table table-
+     light small">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">nombre</th>
+          <th scope="col">apellido</th>
+          <th scope="col">email</th>
+          <th scope="col">pass</th>
+          <th scope="col">fecha</th>
+          <th scope="col">sexo</th>
+          <th scope="col">avatar</th>
+          <th scope="col">perfil</th>
+          <th scope="col">valoracion</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php foreach ($registros as $registro): ?>
+      <tr>
+        <th scope="row"><a href="perfil.php?id=<?=$registro["id"]?>"><?=$registro["id"]?></a></th>
+        <td><?=$registro["nombre"]?></td>
+        <td><?=$registro["apellido"]?></td>
+        <td><?=$registro["email"]?></td>
+        <td><?=$registro["pass"]?></td>
+        <td><?=$registro["fecha"]?></td>
+        <td><?=$registro["sexo"]?></td>
+        <td>
+        <?php
+          if ($registro["avatar"]!=null) {
+            ?>
+            <a href="registros.php?id=<?=$registro["id"]?>"><?=$registro["avatar"]?></a>
+            <?php
+          }else {?>
+            <a href="registros.php?id=<?=$registro["id"]?>">agregar</a>
+
+        <?php } ?>
+        </td>
+
+        <td>
+        <?php
+        if ($registro["perfil"]!=null) {
+          ?>
+          <a href="registros.php?id=<?=$registro["id"]?>"><?=$registro["perfil"]?></a>
+          <?php
+        }else {?>
+          <a href="registros.php?id=<?=$registro["id"]?>">agregar</a>
+        <?php } ?>
+        </td>
+
+        <td><?=$registro["val_user"]?></td>
+      </tr>
+    <?php endforeach; ?>
+    </tbody>
+   </table>
+   </div>
+
+
+   <?php
+   include_once("editarAvatar.php");
+   include_once("javascript.php");
+   ?>
+   <?php if (isset($_GET["id"])): ?>
+     <script type="text/javascript">
+      $(function(){
+       $("#avatar").modal();
+      });
+     </script>
+   <?php endif; ?>
 
 
    </body>
